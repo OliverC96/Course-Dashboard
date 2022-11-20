3348bed10d347edd7b8ead67e11580e4f481f4f7
Thomas S <samduykhanh01@gmail.com>
2022-11-19 19:14:15 -0500

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,51 +1,60 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+def course(program, module):
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
 
-import time
-import requests
-from bs4 import BeautifulSoup
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options=chrome_options)
 
-year_1 = []
-year_2 = []
-year_3 = []
-year_4 = []
+    url = "https://www.westerncalendar.uwo.ca/Modules.cfm"
+    driver.get(url)
 
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=chrome_options)
+    courseList = []
+    program = "Computer Science"  # For testing purposes. Will have to delete later
+    module = "Honours Specialization"  # For testing purposes. Will have to delete later
+    st = module + " in " + program
+    st = st.upper()
 
-url = "https://www.westerncalendar.uwo.ca/Modules.cfm"
-driver.get(url)
+    # Find the search bar and clicks on it
+    search_bar = driver.find_element(
+        By.CSS_SELECTOR, "input[type='search'][class='form-control input-sm']")
+    search_bar.send_keys(program)
 
-program = "Computer Science"  # For testing purposes
-module = "Honours Specialization"  # For testing purposes
-st = module + " in " + program
-st = st.upper()
+    # Click on the module
+    driver.find_element(By.CLASS_NAME, "moduleDeptName").click()
 
-# Find the search bar and clicks on it
-search_bar = driver.find_element(By.CSS_SELECTOR, "input[type='search'][class='form-control input-sm']")
-search_bar.send_keys(program)
+    # Finds the collapse interative thing to open the lists of courses:
+    driver.find_element(
+        By.CSS_SELECTOR, "a[role='button'][data-toggle='collapse']").click()
 
-# Click on the module
-driver.find_element(By.CLASS_NAME, "moduleDeptName").click()
+    # Click on the specific module on the collapse window
+    dir_mod = driver.find_elements(By.XPATH, "//*[@id='collapseOne']/div/div/a")
+    for s in dir_mod:
+        print(s.text)
+        if s.text == st:
+            s.click()
+            break
 
-# Finds the collapse interative thing to open the lists of courses:
-driver.find_element(By.CSS_SELECTOR, "a[role='button'][data-toggle='collapse']").click()
+    # Click on the course:
+    dir_course = driver.find_elements(
+        By.XPATH, "//*[@id='AdmissionRequirements']/div/span/a")
+    # print(dir_course)
+    for s in dir_course:
+        courseList.append(s.text)
+        print(s.text)
 
-# Click on the specific module on the collapse window 
-dir_mod = driver.find_elements(By.XPATH, "//*[@id='collapseOne']/div/div/a")
-for s in dir_mod:
-  print(s.text)
-  if s.text == st:
-    s.click()
+    dir_module = driver.find_elements(
+        By.XPATH, "//*[@id='ModuleInformationDiv']/div[1]/div[9]/div/p/a")
+    # print(dir_module
+    for s in dir_module:
+        courseList.append(s.text)
+        print(s.text)
 
-
-# Click on the course:
-# dir_course = driver.find_elements(By.XPATH, "//*[@id='AdmissionRequirements']/div/span/a")
-
-# for s in dir_course:
-#   print(s.text)
+    for n in courseList:
+        print(n + " ", end='')
+        
+    return courseList
